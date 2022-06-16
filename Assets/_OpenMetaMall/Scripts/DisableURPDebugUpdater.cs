3fb0f913cbe0c9e7cc11fe59fa5b424b4b42dc72
Beytullah Çetin <pasaport_07@hotmail.com>
2022-06-16 17:42:08 +0300

    add DisableURPDebugUpdater

---
+++ Assets/_OpenMetaMall/Scripts/DisableURPDebugUpdater.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using UnityEngine.Rendering;
+ 
+public class DisableURPDebugUpdater : MonoBehaviour
+{
+    private void Awake()
+    {
+        DebugManager.instance.enableRuntimeUI = false;
+    }
+}
