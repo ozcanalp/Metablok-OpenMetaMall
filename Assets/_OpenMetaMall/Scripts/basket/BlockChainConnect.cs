fec45aa7846127a65d8d3d38a10de01106f36e83
Resul <resulsahin@gmail.com>
2022-06-17 16:17:07 +0300

    add Basket Player feature

---
+++ Assets/_OpenMetaMall/Scripts/basket/BlockChainConnect.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+using System.Runtime.InteropServices;
+
+public class BlockChainConnect : MonoBehaviour {
+
+    [DllImport("__Internal")]
+    private static extern void Loaded();
+
+    [DllImport("__Internal")]
+    public static extern void RequestPlugConnect(int cbIndex);
+
+    /* 
+    [DllImport("__Internal")]
+    public static extern void CheckPlugConnection(int cbIndex);
+
+    [DllImport("__Internal")]
+    public static extern int pay(int cbIndex, double to, double amount);*/
+
+    void Start() {
+
+        Debug.Log("Block Chain Connect Start");
+        Loaded();
+
+        /* Hello();
+        
+        HelloString("This is a string.");
+        
+        float[] myArray = new float[10];
+        PrintFloatArray(myArray, myArray.Length);
+        
+        int result = AddNumbers(5, 7);
+        Debug.Log(result);
+        
+        Debug.Log(StringReturnValueFunction());
+        
+        var texture = new Texture2D(0, 0, TextureFormat.ARGB32, false);
+        BindWebGLTexture(texture.GetNativeTexturePtr()); */
+    }
+
+    public void Call_Loaded()
+    {
+        Loaded();
+    }
+}
